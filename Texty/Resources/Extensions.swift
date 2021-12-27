f85b9db9df32de534d690d9c0e509f6658ead08a
jeslinyeoh <jeslinyeoh@gmail.com>
2021-12-27 23:55:53 +0800

    Version 1.2 - Setup Login & Register Screen

---
+++ Texty/Resources/Extensions.swift
@@ -0,0 +1,40 @@
+//
+//  Extensions.swift
+//  Texty
+//
+//  Created by Jeslin Yeoh on 27/12/2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIView{
+    
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return self.frame.size.width + self.frame.origin.x
+    }
+    
+}
+
+
+
